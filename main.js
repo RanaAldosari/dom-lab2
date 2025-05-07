803325e3447bb8055beb2e861e3713cbfe1d2665
rno9i <iirn8944@gmail.com>
2025-05-07 13:51:44 +0300

    done

---
+++ main.js
@@ -0,0 +1,39 @@
+let students = [
+{
+name: "rana",
+age:24,
+gender:"femal",
+imgUrl:"Screenshot 2025-04-30 134600.png",
+    },
+{
+name: "dema",
+gender:"male",
+age:30,
+imgUrl:
+"Screenshot 2025-05-01 104944.png",
+},
+{
+name: "aisha",
+age:20,
+gender:"femal",
+imgUrl:
+"Screenshot 2025-05-01 104944.png",
+    },
+  ];
+  
+  let body = document.body;
+  students.map((img) => {
+    let gend=document.createElement("p");
+    gend.innerText=`user gender : ${img.gender}`
+    if(img.gender==="femal"){
+gend.style.color="pink";
+
+}else{
+        gend.style.color="blue"
+}
+    body.appendChild(gend)
+    let p = document.createElement("p");
+    p.innerText = img.imgUrl;
+    p.innerText = ` Image Url : ${img.imgUrl}`;
+    body.appendChild(p);
+  });
